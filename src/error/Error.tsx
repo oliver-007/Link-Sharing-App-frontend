032eab81fcff486255459fbba846f95b5f671a3e
oliver-007 <oliverahmed000@gmail.com>
2024-11-10 21:23:09 +0600

    refactored error page

--- src/error/Error.tsx
+++ src/error/Error.tsx
@@ -1,29 +1,62 @@
+// import { useNavigate } from "react-router-dom";
+// import error404Svg from "../assets/404_error.svg";
+// import error401Svg from "../assets/401_error.svg";
+
+// interface IErrorProps {
+//   errorMessage?: string;
+//   status?: number;
+// }
+
+// const Error: React.FC<IErrorProps> = ({ errorMessage, status }) => {
+//   const navigate = useNavigate();
+
+//   const handleBtn = () => {
+//     navigate(`${status === 401 ? "/login" : "/links"}`);
+//   };
+
+//   return (
+//     <div
+//       className="flex flex-col items-center justify-center bg-no-repeat bg-center w-screen h-screen  "
+//       style={{
+//         backgroundImage: `url(${status === 401 ? error401Svg : error404Svg})`,
+//       }}
+//     >
+//       <p className="text-3xl text-rose-500 font-semibold"> {errorMessage} </p>
+//       <button onClick={handleBtn} className="btn" type="button">
+//         {status === 401 ? "Reload" : "Go Home"}
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Error;
+
 import { useNavigate } from "react-router-dom";
 import error404Svg from "../assets/404_error.svg";
-import error401Svg from "../assets/401_error.svg";
+// import error401Svg from "../assets/401_error.svg";
 
-interface IErrorProps {
-  errorMessage?: string;
-  status?: number;
-}
+// interface IErrorProps {
+//   errorMessage?: string;
+//   status?: number;
+// }
 
-const Error: React.FC<IErrorProps> = ({ errorMessage, status }) => {
+const Error = () => {
   const navigate = useNavigate();
 
   const handleBtn = () => {
-    navigate(`${status === 401 ? "/login" : "/links"}`);
+    navigate("/links");
   };
 
   return (
     <div
       className="flex flex-col items-center justify-center bg-no-repeat bg-center w-screen h-screen  "
       style={{
-        backgroundImage: `url(${status === 401 ? error401Svg : error404Svg})`,
+        backgroundImage: `url( ${error404Svg})`,
       }}
     >
-      <p className="text-3xl text-rose-500 font-semibold"> {errorMessage} </p>
+      {/* <p className="text-3xl text-rose-500 font-semibold"> {errorMessage} </p> */}
       <button onClick={handleBtn} className="btn" type="button">
-        {status === 401 ? "Reload" : "Go Home"}
+        Go Home
       </button>
     </div>
   );
